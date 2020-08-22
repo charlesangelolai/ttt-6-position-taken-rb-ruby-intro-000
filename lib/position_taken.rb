f8827c4a74770b06fe767e1220613dd62918a1ce
Charles Lai <charlesangeloplai@gmail.com>
2020-08-22 22:19:41 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,4 @@
 # code your #position_taken? method here!
 def position_taken(board)
-  if board[0] != " " || board[1] != " " || board[2] != " "
+  if board[0] == " " || board[1] == " " || board[2] == " "
     return false
