14f5c9030f72b2b2b4891e76aef6cc44a31cbff0
Charles Lai <charlesangeloplai@gmail.com>
2020-08-22 22:33:02 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,7 +4,7 @@ def position_taken?(board, index)
     false
   elsif board[index] == nil
     false
-  elsif board[index] == "X" || board[index] == "Y"
+  elsif board[index] == "X" || board[index] == "O"
     true
   end
 end
